b00156d4b1a49aade3bbe45bf62dc456d71235e4
Lukas Bystricky <lb13f@my.fsu.edu>
2018-09-18 04:36:11 -0400

    christoffel.m: corrected scaling to match with paper

--- src/matlab/exact_cvt/christoffel.m
+++ src/matlab/exact_cvt/christoffel.m
@@ -1,12 +1,18 @@
-function W = christoffel(total_degree, x, y)
+function W = christoffel(total_degree, X)
+
+x = X(:,1);
+y = X(:,2);
 
 index_set = total_degree_set(total_degree);
 
 m = size(index_set,1);
+drho = 0.25;
 
 W = 0;
 for j = 1:m
    W_tmp = sqrt(1 + 2*index_set(j,1))* sqrt(1 + 2*index_set(j,2)) * ...
         polyval(LegendrePoly(index_set(j,1)),x) .* polyval(LegendrePoly(index_set(j,2)),y) ;
-   W = W + W_tmp.^2;
+   W = W + W_tmp.^2 * drho;
 end
+
+W = W/m;
